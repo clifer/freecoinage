f8faf26f06cd5d44148de08e9d7ef3dd35fbe520
Darren Ross <darren0170@gmail.com>
2016-06-12 17:27:25 -0500

    yet more movement

--- freecoinage/coins/currencys/urls.py
+++ freecoinage/coins/currencys/urls.py
@@ -3,17 +3,17 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
 
-from freecoinage.coins.view import currency
+from .views import *
 
 urlpatterns = [
     url(
         regex=r'^currency/$',
-        view=currency.currencyListView.as_view(),
+        view=currencyListView.as_view(),
         name='currencyList'
     ),
     url(
         regex=r'^currency/(?P<slug>[\w|\W]+)$',
-        view=currency.currencyDetailView.as_view(),
+        view=currencyDetailView.as_view(),
         name='currencyDetail'
     ),
 
