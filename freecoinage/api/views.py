257981d3b14381482b6cff7cbd3bc8c1639799d4
Darren Ross <darren0170@gmail.com>
2016-06-12 18:31:58 -0500

    fix

--- freecoinage/api/views.py
+++ freecoinage/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from freecoinage.coins.models import Market
-from api.serializers import marketSerializer
+from freecoinage.api.serializers import marketSerializer
 
 
 @api_view(['GET', 'POST'])
