7280c6d49dd7290dce5a0384f66c50dafbf545c5
Darren Ross <darren0170@gmail.com>
2016-06-18 06:13:42 -0500

    more

--- freecoinage/coins/admin.py
+++ freecoinage/coins/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from .models import  Coin, Currency, Algorithm, Miner, MiningPool, Market, Daemon, Exchange
+from .models import  Coin, Currency, Algorithm, Miner, MiningPool, MiningPoolType, Market, Daemon, Exchange
 #from .models import UserCoin, Coin, Currency, Algorithm, Miner, MiningPool, MiningPoolType,  Market, Daemon, Exchange
 from freecoinage.users.models import UserCoin
 
@@ -11,8 +11,8 @@ admin.site.register(UserCoin)
 admin.site.register(Currency)
 admin.site.register(Algorithm)
 admin.site.register(Miner)
-admin.site.register(MiningPool)
 admin.site.register(MiningPoolType)
+admin.site.register(MiningPool)
 admin.site.register(Market)
 admin.site.register(Daemon)
 admin.site.register(Exchange)
